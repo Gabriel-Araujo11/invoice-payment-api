9cdac756a370cd471471faea46947672d4af9ac0
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2025-06-30 15:37:52 -0300

    fix: last overload error

--- src/controllers/billController.ts
+++ src/controllers/billController.ts
@@ -2,15 +2,16 @@ import { Request, Response } from 'express';
 import { calculateBillValues } from '../services/billService';
 import { billSchema } from '../schema/billSchema';
 
-export function calculateBill(req: Request, res: Response) {
+export const calculateBill = (req: Request, res: Response): void => {
     try {
         const validated = billSchema.parse(req.body);
         const data = calculateBillValues(validated);
-        return res.status(200).json(data);
+        res.status(200).json(data);
     } catch (error: any) {
         if (error.name === 'ZodError') {
-            return res.status(400).json({ error: error.errors });
+            res.status(400).json({ error: error.errors });
+            return;
         }
-        return res.status(500).json({ error: 'Internal server error' });
+        res.status(500).json({ error: 'Internal server error' });
     }
-}
+};
